dc4051aaaa8da860dbc861be2ced70033cdc8eac
Adarsh2904 <adarshsinha290@gmail.com>
2026-06-06 13:05:36 +0530

    test

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//test
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/vendorbridge');
