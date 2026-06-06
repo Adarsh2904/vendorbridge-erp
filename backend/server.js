c7a2173773a3310cae1cd219ad78e17e9689b875
smit <smitshah22052003@gmail.com>
2026-06-06 10:54:28 +0530

    some bug resolve in backend

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-require('./config/db').then(() => {
+require('./config/db')().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
